0b9ba161998e25a7dfda9510ca543da99434a1f1
Theo F <theo.farnole@gmail.com>
2022-10-08 12:56:06 +0200

    FIX manifest.json unknow file

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     mode: "production",
     entry: {
-        background: path.resolve(__dirname, "..", "src", "content.ts"),
+        content: path.resolve(__dirname, "..", "src", "content.ts"),
     },
     output: {
         path: path.join(__dirname, "../dist"),
