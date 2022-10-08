476e040a060388aefb90e0a3c63c7c55f89aeefd
Theo F <theo.farnole@gmail.com>
2022-10-08 13:01:20 +0200

    ADD getNft function

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,19 @@
-console.log("Hello world");
+
+function main() {
+    console.log("Current NFT:", getNft());
+
+    const nft = getNft();
+    const attribute = getAttribute(nft);
+
+    console.log(`Attribute for ${nft}:`, attribute);
+}
+
+async function getAttribute(nft: string): Promise<Buffer> {
+    throw new Error("Not implemented");
+}
+
+function getNft(): string {
+    const splitted = window.location.href.split("/")
+
+    return splitted[splitted.length - 1];
+}
