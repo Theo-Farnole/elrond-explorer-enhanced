16824fd1a2ee705798878c153f0df764bd25e8d5
Theo F <theo.farnole@gmail.com>
2022-10-08 15:55:41 +0200

    ADD addCard()

--- src/content.ts
+++ src/content.ts
@@ -38,20 +38,30 @@ function setAttributeToHtml(attribute: Buffer) {
 function addCard(name: string, value: string) {
     const container = getContainer();
 
+    container.innerHTML = container.innerHTML + `
+<div class="row detail-item  border-bottom py-3">
+    <div class="col-lg-2 text-secondary text-lg-right pl-lg-spacer">
+        ${name}
+    </div>
+    <div class="col-lg-10 pr-lg-spacer text-break">
+        ${value}
+    </div>
+</div>`
+
 }
 
 function getContainer(): Element {
-    const cardBody = document.getElementsByClassName("card-body")[0];
+    const container = document.getElementsByClassName("page-content")[0]
+        ?.children[0] // row
+        ?.children[0] // col-12
+        ?.children[0] // card
+        ?.getElementsByClassName("card-body")[0]
+        ?.children[0];
 
-    if (!cardBody) {
-        throw new Error("Could not find card-body");
+    if (container == null) {
+        throw new Error("Could not find card body");
     }
 
-    const containerFluid = cardBody.getElementsByClassName("container-fluid")[0];
 
-    if (!containerFluid) {
-        throw new Error("Could not find container-fluid");
-    }
-
-    return containerFluid;
+    return container;
 }
